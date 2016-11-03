3b20afd7f13df39dd530a76f06c22b6af5a2e4fd
laxusjames <laxusjames@gmail.com>
2016-11-03 14:37:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 
 $server = $_SERVER['HTTP_HOST'];
+$show = $_SERVER['SERVER_NAME'];
+$yas = $_SERVER['SERVER_ADDR'];
+$pas = $_SERVER['SERVER'];
+
+
+echo $show;
+echo $yas;
+echo $pas;
 echo $server;
 
 
